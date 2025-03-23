69780ce91bd54ad089a651ac2bb5e0a77a70a2af
maybenicko <tronconiniccolo@gmail.com>
2025-03-23 14:28:23 +0100

    ip change test

--- server/app.py
+++ server/app.py
@@ -2,8 +2,6 @@ from flask import Flask, jsonify
 import sqlite3
 from flask_cors import CORS
 import os
-import json
-import ast
 
 
 app = Flask(__name__)
@@ -75,4 +73,4 @@ def get_politics_data():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="192.168.1.7", port=5000)
