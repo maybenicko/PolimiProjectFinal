35b20fcfb76f301394e4b6f207f7a347cc7974a1
maybenicko <tronconiniccolo@gmail.com>
2025-03-24 12:05:40 +0100

    bug fixes

--- static/js/nft_change.js
+++ static/js/nft_change.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     const imageElement = document.getElementById("dynamic-image");
-    const totalImages = 59; // Images range from 0.png to 58.png
-  
+    const totalImages = 58; // Images range from 1.png to 58.png
+
     function getRandomImage() {
-      const randomIndex = Math.floor(Math.random() * totalImages);
+      const randomIndex = Math.floor(Math.random() * totalImages) + 1; // Ensure range is 1 to 58
       return `static/images/${randomIndex}.png`;
     }
   
     function changeImage() {
       imageElement.style.opacity = 0; // Fade out
-  
+
       setTimeout(() => {
         imageElement.src = getRandomImage();
         imageElement.style.opacity = 1; // Fade in
       }, 1000); // Transition timing (matches CSS)
     }
-  
-    // Change image every 2.5 seconds
+
+    // Change image every 5 seconds
     setInterval(changeImage, 5000);
   });
